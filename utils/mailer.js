bd90090c621bb428882b6789910b2127eb05fd6d
Abhay gupta <guptaabhay37075@gmail.com>
2026-06-26 13:45:27 +0530

    added new file

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,15 +3,16 @@ console.log("EMAIL_USER =", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
 
 
+
+
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 transporter.verify((err, success) => {
