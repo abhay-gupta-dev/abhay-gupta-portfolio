6f6e34ba74b2ec2c23814fb105ec9f1d62f14a2c
Abhay gupta <guptaabhay37075@gmail.com>
2026-06-26 13:42:54 +0530

    Downgrade Nodemailer to stable version

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,10 +1,24 @@
 const nodemailer = require("nodemailer");
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
+
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+transporter.verify((err, success) => {
+    if (err) {
+        console.error("SMTP VERIFY ERROR:", err);
+    } else {
+        console.log("SMTP READY");
     }
 });
 
